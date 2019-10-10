cb65f37e7ecf6acd9d0a7f4a3e5e7201ac53e9e9
Grafton Cook <grafton.cook@geappliances.com>
2019-10-10 11:16:10 -0400

    Added comments and cleaned variable names

--- filesearch.py
+++ filesearch.py
@@ -2,17 +2,21 @@ import os, csv
 
 filelist = []
 ignore_directories = 'data/mb/washer', 'data/sb', 'spec', 'tools', 'build-scripts'
-show_results = 'all' #use 'all' to see all results or specify a number
+show_results = 'all' # Use 'all' to see all results or specify a number
 
+# Crawl directories to create a master file list.
 for dirpath, dirnames, filenames in os.walk('.'):
-  for filename in [x for x in filenames if x.endswith('.lua')]:
-    filepath = os.path.join(dirpath, filename)
+  for fn in [x for x in filenames if x.endswith('.lua')]:
+    filepath = os.path.join(dirpath, fn)
     filepath = filepath[2:]
     filelist.append(filepath)
+
+# Filter the file list by removing files from ignored directories.
 filtered_filelist = [y for y in filelist if not y.startswith(ignore_directories)]
 
-with open('listtest.csv', mode='w') as listDude:
-  listWriter=csv.writer(listDude, delimiter=',')
+# Create a CSV with the filenames and import counts
+with open('hitlist.csv', mode='w') as cleaner:
+  listWriter=csv.writer(cleaner, delimiter=',')
   for filepath in filtered_filelist:
     count = 0
     for searchfile in filtered_filelist:
@@ -20,9 +24,11 @@ with open('listtest.csv', mode='w') as listDude:
         if filepath in myfile.read():
           count = count + 1
     if show_results == 'all':
+      # Uncomment the line below to echo program output to stdout.
       # print 'Total usage for', filepath, count
       row = [filepath, count]
       listWriter.writerow(row)
     else:
       if count == show_results:
+        # TODO: Create this feature.
         print 'Total usage for', filepath, count
